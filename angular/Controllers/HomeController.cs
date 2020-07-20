7171419117a8c89239b9a4a65c97ce93ae8c62a8
John Deepak <johnd@techaffinity.com>
2020-07-20 22:43:46 +0530

    Add project files.

---
+++ angular/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace angular.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+         public JsonResult Get_AllEmployee() {  
+                using(praticeEntities Obj = new praticeEntities()) {  
+                    List <praticejoin> Emp = Obj.praticejoins.ToList();  
+                    return Json(Emp, JsonRequestBehavior.AllowGet);  
+                }  
+            }  
+            /// <summary>  
+            /// Get praticejoin With Id  
+            /// </summary>  
+            /// <param name="Id"></param>  
+            /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id) {  
+                using(praticeEntities Obj = new praticeEntities()) {  
+                    int EmpId = int.Parse(Id);  
+                    return Json(Obj.praticejoins.Find(EmpId), JsonRequestBehavior.AllowGet);  
+                }  
+            }  
+            /// <summary>  
+            /// Insert New praticejoin  
+            /// </summary>  
+            /// <param name="Employe"></param>  
+            /// <returns></returns>  
+        public string Insert_Employee(praticejoin Employe) {  
+                if (Employe != null) {  
+                    using(praticeEntities Obj = new praticeEntities()) {  
+                        Obj.praticejoins.Add(Employe);  
+                        Obj.SaveChanges();  
+                        return "praticejoin Added Successfully";  
+                    }  
+                } else {  
+                    return "praticejoin Not Inserted! Try Again";  
+                }  
+            }  
+            /// <summary>  
+            /// Delete praticejoin Information  
+            /// </summary>  
+            /// <param name="Emp"></param>  
+            /// <returns></returns>  
+        public string Delete_Employee(praticejoin Emp) {  
+                if (Emp != null) {  
+                    using(praticeEntities Obj = new praticeEntities()) {  
+                        var Emp_ = Obj.Entry(Emp);  
+                        if (Emp_.State == System.Data.Entity.EntityState.Detached) {  
+                            Obj.praticejoins.Attach(Emp);  
+                            Obj.praticejoins.Remove(Emp);  
+                        }  
+                        Obj.SaveChanges();  
+                        return "praticejoin Deleted Successfully";  
+                    }  
+                } else {  
+                    return "praticejoin Not Deleted! Try Again";  
+                }  
+            }  
+            /// <summary>  
+            /// Update praticejoin Information  
+            /// </summary>  
+            /// <param name="Emp"></param>  
+            /// <returns></returns>  
+        public string Update_Employee(praticejoin Emp) {  
+            if (Emp != null) {  
+                using(praticeEntities Obj = new praticeEntities()) {  
+                    var Emp_ = Obj.Entry(Emp);  
+                    praticejoin EmpObj = Obj.praticejoins.Where(x => x.Id == Emp.Id).FirstOrDefault();  
+                    EmpObj.OrderNumber = Emp.OrderNumber;  
+                    EmpObj.OrderDate = Emp.OrderDate;  
+                    EmpObj.CustomerId = Emp.CustomerId;  
+                    Obj.SaveChanges();  
+                    return "praticejoin Updated Successfully";  
+                }  
+            } else {  
+                return "praticejoin Not Updated! Try Again";  
+            }  
+        }  
+    }  
+}  
+   
---
+++ angular/Scripts/AngularCode.js
@@ -0,0 +1,83 @@
+﻿
+
+   
+
+var app = angular.module('myapp', []);
+app.controller("HomeController", function ($scope, $http) {
+    debugger;
+    $scope.InsertData = function () {
+        debugger
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employe = {};
+            $scope.Employe.OrderDate = $scope.EmpName;
+            $scope.Employe.CustomerId = $scope.EmpCity;
+            $scope.Employe.Id = $scope.EmpAge;
+            $http({
+                method: "POST",
+                url: "/Home/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+            })
+        } else {
+            $scope.Employe = {};
+            $scope.Employe.OrderDate = $scope.EmpName;
+            $scope.Employe.CustomerId = $scope.EmpCity;
+            $scope.Employe.Id = $scope.EmpAge;
+            $scope.Employe.Emp_Id = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "http://localhost:1316/Home/Update_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+
+        $http({
+            method: "get",
+            url: "http://localhost:1316/Home/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "http://localhost:1316/Home/Delete_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        debugger
+        document.getElementById("EmpID_").value = Emp.Emp_Id;
+        $scope.EmpName = Emp.OrderDate;
+        $scope.EmpCity = Emp.CustomerId;
+        $scope.EmpAge = Emp.Id;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Yellow";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})
